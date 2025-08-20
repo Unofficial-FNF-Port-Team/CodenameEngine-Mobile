074f16be91951288b227bb8a5c5d311211bb7b6d
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-08-20 14:06:34 +0300

    fuck

---
+++ source/funkin/mobile/utils/MobileUtil2.hx
@@ -0,0 +1,77 @@
+package funkin.mobile.utils;
+
+#if android
+import extension.androidtools.os.Build.VERSION;
+import extension.androidtools.os.Environment;
+import extension.androidtools.Permissions;
+import extension.androidtools.Settings;
+
+import lime.system.System;
+import lime.app.Application;
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
+
+using StringTools;
+
+/** 
+* @author MaysLastPlay, MarioMaster (MasterX-39)
+* @version: 0.2.2
+**/
+
+class MobileUtil {
+  public static var currentDirectory:String = null;
+
+  /**
+   * Get the directory for the application. (External for Android Platform and Internal for iOS Platform.)
+   */
+
+  public static function getDirectory():String {
+   currentDirectory = if (VERSION.SDK_INT >= 33) Environment.getExternalStorageDirectory() + '/.' + Application.current.meta.get('file') else Environment.getExternalStorageDirectory() + '/Android/media/' + lime.app.Application.current.meta.get('packageName');
+
+  return currentDirectory;
+  }
+
+  /**
+   * Requests Storage Permissions on Android Platform.
+   */
+  
+    public static function getPermissions():Void
+    {
+    if(VERSION.SDK_INT >= 33){
+	   if (!Environment.isExternalStorageManager()) {
+	     Settings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+	   }
+      } else {
+      Permissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
+	  }
+
+    try {
+      if(!FileSystem.exists(MobileUtil.getDirectory()))
+        FileSystem.createDirectory(MobileUtil.getDirectory());
+     } catch (e:Dynamic) {
+    trace(e);
+  if(!FileSystem.exists(MobileUtil.getDirectory())) {
+    NativeAPI.showMessageBox("Seems like you didnt enabled permissions requested to run the game.", 'Uncaught Error');
+    FileSystem.createDirectory(MobileUtil.getDirectory());
+     System.exit(0);
+     }
+    }
+  }
+
+  /**
+   * Saves a file to the external storage.
+   */
+
+	public static function save(fileName:String = 'Ye', fileExt:String = '.txt', fileData:String = 'Nice try, but you failed, try again!')
+	{
+	  var savesDir:String = MobileUtil.getDirectory() + 'saves/';
+
+		if (!FileSystem.exists(savesDir))
+			FileSystem.createDirectory(savesDir);
+
+		File.saveContent(savesDir + fileName + fileExt, fileData);
+	}
+}
+#end
