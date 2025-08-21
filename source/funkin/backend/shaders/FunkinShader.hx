b97b0c96fced48d08afd3913159d1d6d2c7eee99
Dxgamer7405 <dechis7405au@gmail.com>
2025-08-21 12:24:07 -0300

    Shader Logs

--- source/funkin/backend/shaders/FunkinShader.hx
+++ source/funkin/backend/shaders/FunkinShader.hx
@@ -1,5 +1,6 @@
 package funkin.backend.shaders;
 
+import funkin.backend.utils.NativeAPI;
 import flixel.graphics.FlxGraphic;
 import flixel.system.FlxAssets.FlxShader;
 import flixel.util.FlxSignal.FlxTypedSignal;
@@ -153,7 +154,13 @@ class FunkinShader extends FlxShader implements IHScriptCustomBehaviour {
 			messageBuf.add(source);
 
 			var message = messageBuf.toString();
-			if (compileStatus == 0) Log.error(message);
+			if (compileStatus == 0)
+			{
+				#if mobile
+                NativeAPI.showMessageBox("Shader Compile Error!", message, MSG_ERROR);
+				#end
+				Log.error(message);
+			}
 			else if (hasInfoLog) Log.debug(message);
 		}
 
@@ -193,6 +200,9 @@ class FunkinShader extends FlxShader implements IHScriptCustomBehaviour {
 				messageBuf.add("\n");
 				messageBuf.add(gl.getProgramInfoLog(program));
 				var message = messageBuf.toString();
+				#if mobile
+                NativeAPI.showMessageBox("Shader Compile Error!", message, MSG_ERROR);
+				#end
 				Log.error(message);
 			}
 		}
@@ -203,6 +213,9 @@ class FunkinShader extends FlxShader implements IHScriptCustomBehaviour {
 				Logs.logText('Failed to compile shader ${fileName}: ', RED),
 				Logs.logText(Std.string(error))
 			], TRACE);
+			#if mobile
+            NativeAPI.showMessageBox("Shader Compile Error!", 'Failed to compile shader ${fileName}: \n${Std.string(error)}', MSG_ERROR);
+			#end
 		}
 		return program;
 
@@ -283,6 +296,9 @@ class FunkinShader extends FlxShader implements IHScriptCustomBehaviour {
 
 			var gl = __context.gl;
 
+			#if (js && html5)
+			prefixBuf.add(precisionHint == FULL ? "precision mediump float;\n" : "precision lowp float;\n");
+			#else
 			prefixBuf.add("#ifdef GL_ES\n");
 			if (precisionHint == FULL) {
 				prefixBuf.add("#ifdef GL_FRAGMENT_PRECISION_HIGH\n");
@@ -294,6 +310,7 @@ class FunkinShader extends FlxShader implements IHScriptCustomBehaviour {
 				prefixBuf.add("precision lowp float;\n");
 			}
 			prefixBuf.add("#endif\n");
+			#end
 
 			var prefix = prefixBuf.toString();
 
--- source/funkin/backend/system/Flags.hx
+++ source/funkin/backend/system/Flags.hx
@@ -261,7 +261,7 @@ class Flags {
 	public static var DEFAULT_CHARACTER_GHOSTDISABLE_SOUND:String = "editors/character/ghostDisable";
 	public static var DEFAULT_CHARACTER_GHOSTENABLE_SOUND:String = "editors/character/ghostEnable";
 
-	public static var DEFAULT_GLSL_VERSION:String = "120";
+	public static var DEFAULT_GLSL_VERSION:String = #if mobile "100" #else "120" #end;
 	@:also(funkin.backend.utils.HttpUtil.userAgent)
 	public static var USER_AGENT:String = 'request';
 	// -- End of Codename's Default Flags --
