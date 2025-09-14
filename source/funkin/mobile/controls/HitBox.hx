23dc211e44d724bdc1ef8bf61718687be16609f5
Cream.BR <creambr6@gmail.com>
2025-09-14 13:09:36 -0300

    I forgot about that lol

--- source/funkin/mobile/controls/HitBox.hx
+++ source/funkin/mobile/controls/HitBox.hx
@@ -41,12 +41,12 @@ class HitBox extends FlxSpriteGroup
 
         if (canExtraButton) {
             if (canSecondExtraButton) {
-                buttonExtraOne = createHitbox(0, Std.int(FlxG.height * 0.75), Std.int(FlxG.width / 2), Std.int(FlxG.height * 0.25), '0xFFFFFF', true);
+                buttonExtraOne = createHitbox(0, Std.int(FlxG.height * 0.75), Std.int(FlxG.width / 2), Std.int(FlxG.height * 0.25), '0xFFFFFF');
                 add(buttonExtraOne);
-                buttonExtraTwo = createHitbox(FlxG.width / 2, Std.int(FlxG.height * 0.75), Std.int(FlxG.width / 2), Std.int(FlxG.height * 0.25), '0xFFFF00', true);
+                buttonExtraTwo = createHitbox(FlxG.width / 2, Std.int(FlxG.height * 0.75), Std.int(FlxG.width / 2), Std.int(FlxG.height * 0.25), '0xFFFF00');
                 add(buttonExtraTwo);
             } else {
-                buttonExtra = createHitbox(0, Std.int(FlxG.height * 0.75), FlxG.width, Std.int(FlxG.height * 0.25), '0xFFFFFF', true);
+                buttonExtra = createHitbox(0, Std.int(FlxG.height * 0.75), FlxG.width, Std.int(FlxG.height * 0.25), '0xFFFFFF');
                 add(buttonExtra);
             }
         }
