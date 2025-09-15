b742a75f97583f42485cf991fd15f1fbd5166f4b
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-09-15 22:08:26 +0300

    probably?
    i wanna eep-

--- source/funkin/backend/system/framerate/Framerate.hx
+++ source/funkin/backend/system/framerate/Framerate.hx
@@ -55,7 +55,7 @@ class Framerate extends Sprite {
 
 		FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, function(e:KeyboardEvent) {
 			switch(e.keyCode) {
-				case #if web Keyboard.NUMBER_3 #else Keyboard.F3 #end: // 3 on web or F3 on windows, linux and other things that runs code
+				case #if web Keyboard.NUMBER_3 #elseif mobile TouchInput.justPressed(bgSprite) #else Keyboard.F3 #end: // 3 on web or F3 on windows, linux and other things that runs code
 					debugMode = (debugMode + 1) % 3;
 			}
 		});
