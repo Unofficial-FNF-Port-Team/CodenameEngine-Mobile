d33e28c040bc5f1a7de3b4c84bd851cbabb43508
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-09-16 12:05:29 +0300

    was this always here?

--- source/funkin/editors/EditorTreeMenu.hx
+++ source/funkin/editors/EditorTreeMenu.hx
@@ -10,7 +10,7 @@ class EditorTreeMenu extends funkin.options.TreeMenu {
 
 	override function create() {
 		super.create();
-		UIState.setResolutionAware();
+		if (Options.editorsResizable) UIState.setResolutionAware();
 		FlxG.camera.fade(0xFF000000, 0.5, true);
 	}
 
