371980d29edba19ac2cee3f0370718a21f5cbca5
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-09-16 16:13:06 +0300

    Update UIFileExplorer.hx

--- source/funkin/editors/ui/UIFileExplorer.hx
+++ source/funkin/editors/ui/UIFileExplorer.hx
@@ -31,7 +31,7 @@ class UIFileExplorer extends UISliceSprite {
 			fileDialog.browse(OPEN, this.fileType);
 			#elseif android
 			FileChooser.openFilePicker();
-			loadFile(file);
+			loadFile(MobileUtil.getDirectory());
 			#end
 		}, bWidth - 16, bHeight - 16);
 		members.push(uploadButton);
